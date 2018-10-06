09320241801d481a5255677c27f6bcb48ab7d29a
Saraswathi05031996 <43901602+Saraswathi05031996@users.noreply.github.com>
2018-10-06 13:51:49 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,20 +5,20 @@ public class Main
     {
         int number;
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the number you want to check:");
+        System.out.print(" ");
         number = scan.nextInt();
         scan.close();
         if(number > 0)
         {
-            System.out.println(number+" is positive number");
+            System.out.println("The given number"+n" is positive number");
         }
         else if(number < 0)
         {
-            System.out.println(number+" is negative number");
+            System.out.println("The given number"+n" is negative number");
         }
         else
         {
-            System.out.println(number+" is neither positive nor negative");
+            System.out.println("The given number"+n" is neither positive nor negative");
         }
     }
 }
